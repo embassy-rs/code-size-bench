2cf13aabec0f9744f4037dd5c32d2ca8e0fdb6cf
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-03-24 20:57:19 +0100

    Remove unused timer from drogue-device example

--- drogue-device/src/device.rs
+++ drogue-device/src/device.rs
@@ -1,41 +1,31 @@
 use drogue_device::{
-    api::{delayer::*, uart::*},
-    domain::time::duration::Milliseconds,
+    api::{uart::*},
     driver::{
-        led::*,
-        timer::*,
         uart::{serial_rx::*, serial_tx::*},
     },
     platform::cortex_m::nrf::{
-        timer::Timer as HalTimer,
         uarte::{UarteRx, UarteTx},
     },
     prelude::*,
 };
-use hal::gpio::{Output, Pin, PushPull};
-use hal::pac::{TIMER0, UARTE0};
-use heapless::consts;
 use nrf52833_hal as hal;
+use hal::pac::UARTE0;
 
-pub type AppTimer = Timer<HalTimer<TIMER0>>;
 pub type AppTx = SerialTx<UarteTx<UARTE0>>;
-pub type AppRx = SerialRx<App<AppTx, <AppTimer as Package>::Primary>, UarteRx<UARTE0>>;
+pub type AppRx = SerialRx<App<AppTx>, UarteRx<UARTE0>>;
 
 pub struct MyDevice {
-    pub timer: AppTimer,
     pub tx: ActorContext<AppTx>,
     pub rx: InterruptContext<AppRx>,
-    pub app: ActorContext<App<AppTx, <AppTimer as Package>::Primary>>,
+    pub app: ActorContext<App<AppTx>>,
 }
 
 impl Device for MyDevice {
     fn mount(&'static self, _: DeviceConfiguration<Self>, supervisor: &mut Supervisor) {
-        let timer = self.timer.mount((), supervisor);
         let uart = self.tx.mount((), supervisor);
         let app = self.app.mount(
             AppConfig {
                 uart,
-                timer,
             },
             supervisor,
         );
@@ -44,45 +34,37 @@ impl Device for MyDevice {
     }
 }
 
-pub struct AppConfig<U, D>
+pub struct AppConfig<U>
 where
     U: UartWriter + 'static,
-    D: Delayer + 'static,
 {
     uart: Address<U>,
-    timer: Address<D>,
 }
 
-pub struct App<U, D>
+pub struct App<U>
 where
     U: UartWriter + 'static,
-    D: Delayer + 'static,
 {
     uart: Option<Address<U>>,
-    timer: Option<Address<D>>,
 }
 
-impl<U, D> App<U, D>
+impl<U> App<U>
 where
     U: UartWriter + 'static,
-    D: Delayer,
 {
     pub fn new() -> Self {
         Self {
             uart: None,
-            timer: None,
         }
     }
 }
-impl<U, D> Actor for App<U, D>
+impl<U> Actor for App<U>
 where
     U: UartWriter + 'static,
-    D: Delayer,
 {
-    type Configuration = AppConfig<U, D>;
+    type Configuration = AppConfig<U>;
     fn on_mount(&mut self, _: Address<Self>, config: Self::Configuration) {
         self.uart.replace(config.uart);
-        self.timer.replace(config.timer);
     }
 
     fn on_start(self) -> Completion<Self> {
@@ -92,10 +74,9 @@ where
     }
 }
 
-impl<U, D> NotifyHandler<SerialData> for App<U, D>
+impl<U> NotifyHandler<SerialData> for App<U>
 where
     U: UartWriter + 'static,
-    D: Delayer + 'static,
 {
     fn on_notify(self, event: SerialData) -> Completion<Self> {
         Completion::defer(async move {
--- drogue-device/src/main.rs
+++ drogue-device/src/main.rs
@@ -7,17 +7,13 @@ use panic_halt as _;
 
 use cortex_m_rt::{entry, exception};
 use drogue_device::{
-    domain::time::rate::Extensions,
-    driver::timer::Timer,
     driver::uart::{serial_rx::*, serial_tx::*},
     platform::cortex_m::nrf::{
-        timer::Timer as HalTimer,
         uarte::{Baudrate, Parity, Pins, Uarte},
     },
     prelude::*,
 };
 use hal::gpio::Level;
-use heapless::{consts, Vec};
 
 use nrf52833_hal as hal;
 
@@ -27,13 +23,9 @@ fn configure() -> MyDevice {
     let device = hal::pac::Peripherals::take().unwrap();
 
     let port0 = hal::gpio::p0::Parts::new(device.P0);
-    let port1 = hal::gpio::p1::Parts::new(device.P1);
 
     let clocks = hal::clocks::Clocks::new(device.CLOCK).enable_ext_hfosc();
     let _clocks = clocks.start_lfclk();
-
-    // Timer
-    let timer = Timer::new(HalTimer::new(device.TIMER0), hal::pac::Interrupt::TIMER0);
 
     // Uart
     static mut RX_BUF: [u8; 1] = [0; 1];
@@ -53,7 +45,6 @@ fn configure() -> MyDevice {
     let rx = SerialRx::new(rx);
 
     MyDevice {
-        timer,
         tx: ActorContext::new(tx).with_name("uart_tx"),
         rx: InterruptContext::new(rx, hal::pac::Interrupt::UARTE0_UART0).with_name("uart_rx"),
         app: ActorContext::new(App::new()),
@@ -62,5 +53,5 @@ fn configure() -> MyDevice {
 
 #[entry]
 fn main() -> ! {
-    device!(MyDevice = configure; 8192);
+    device!(MyDevice = configure; 2048);
 }
